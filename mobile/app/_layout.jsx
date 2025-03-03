5d4b49ddfd09663c15f8d617b8c1f99b6919d6d0
miclemabasie <miclemabasie3@gmail.com>
2025-03-03 07:37:16 +0100

    added clerk

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,16 +1,36 @@
-import { Stack } from "expo-router";
-import { useFonts, useFronts } from "expo-font"
+import { tokenCache } from '@/cache';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { ClerkProvider, ClerkLoaded, useAuth } from '@clerk/clerk-expo';
+import { Text, View } from 'react-native';
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  );
+}
+
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
 
-  const [loaded] = useFonts({
-    "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf")
-  })
+  if (!fontsLoaded) {
+    return <Text>Fonts Loading</Text>; // Return a loading indicator or splash screen
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login/index" />
-      <Stack.Screen name="home/index" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="login/index" />
+          <Stack.Screen name="home/index" />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
